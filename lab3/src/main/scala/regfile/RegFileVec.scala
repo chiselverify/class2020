8c0f41a8b78629f03f5ab50de1a190e5ee01c520
Victor Alexander Hansen <66786672+Vict0rAH@users.noreply.github.com>
2020-09-21 21:27:59 +0200

    Add files via upload

--- lab3/src/main/scala/regfile/RegFileVec.scala
+++ lab3/src/main/scala/regfile/RegFileVec.scala
@@ -3,19 +3,28 @@ import chisel3._
 class RegFileVec(addrWidth: Int, dataWidth: Int) extends RegFile(addrWidth, dataWidth) {
     // The register file is represented as a vector of 2^addrwidth registers
     // of which location zero is never accessed
-    val rf = Reg(Vec(1 << addrWidth, UInt(dataWidth.W)))
+    val rf = Reg(Vec( 1 << addrWidth, UInt(dataWidth.W)))
 
     // Synchronous reads are the result of storing the read addresses
     // in two registers and using their outputs for addressing
     val addrReg1 = RegNext(io.registerRs1)
     val addrReg2 = RegNext(io.registerRs2)
 
-    // Store the data on writeData when regWrite is asserted
-    when (io.regWrite && io.registerRd =/= 0.U) {
+    //Synchronous write to register file
+    when(io.regWrite === 1.U && io.registerRd =/= 0.U){
         rf(io.registerRd) := io.writeData
     }
 
-    // Output data based on the read addresses
-    io.data1 := Mux(addrReg1 === 0.U, 0.U, rf(addrReg1))
-    io.data2 := Mux(addrReg2 === 0.U, 0.U, rf(addrReg2))
+    // Output data 1
+    when(addrReg1 === 0.U){
+        io.data1 := 0.U
+    } .otherwise {
+        io.data1 := rf(addrReg1)
+    }
+    // Output data 2
+    when(addrReg2 === 0.U){
+        io.data2 := 0.U
+    } .otherwise {
+        io.data2 := rf(addrReg2)
+    }
 }
