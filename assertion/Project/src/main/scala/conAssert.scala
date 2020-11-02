d94fe37d3e476e62cb7004adc7645fab784ed6e8
VoldsommeViggo <s194027@student.dtu.dk>
2020-11-02 20:21:46 +0100

    Added conAssert

---
+++ assertion/Project/src/main/scala/conAssert.scala
@@ -0,0 +1,18 @@
+object conAssert {
+    /** Checks for a condition to be valid in the circuit concurrently with time.
+      * If the condition evaluates to false, the test stops with an error
+      * 
+      * @param cond condition, assertion fires (simulation fails) when false
+      * @param cycles number of clock-cycles the assertions checks condition for
+      * @param message optional format string to print when the assertion fires
+      * @param data optional bits to print in the message formatting
+      * 
+      * @note This object is part 
+      *
+      *
+      *
+      *
+      *
+      
+    */
+}
