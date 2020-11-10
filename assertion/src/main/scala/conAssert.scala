54e6d786406e98d7f46a758ab25388faf60e6598
VoldsommeViggo <s194027@student.dtu.dk>
2020-11-10 14:56:06 +0100

    Update conAssert with test

--- assertion/src/main/scala/conAssert.scala
+++ assertion/src/main/scala/conAssert.scala
@@ -1,5 +1,7 @@
 import chisel3._
 import chisel3.util._
+import chiseltest._
+import org.scalatest._
 
 /*object conAssert {
   /** Checks for a condition to be valid in the circuit at all times, or within
@@ -22,24 +24,75 @@ import chisel3.util._
 Fjern udkommentering når vi kan se at klassen virker som forventet.
 */
 
-class conAssert(sigB: Bool, cycles: Int, message: String) extends Module{
+class conAssert(cond: Bool, cycles: Int, message: String) extends Module{
   val io = IO(new Bundle {
     val sigA = Input(Bool())
     val testp = Output(Bool())
   })
   io.testp := false.B
-  
-  when (io.sigA) {
-    for (i <- 0 until cycles) {
-      // Den helt generelle assertion
-      when (sigB){
-        io.testp := true.B
-      } .otherwise {
-        println(message) //Printer uanset
-        io.testp := false.B
+  // The use of fork{} enables the assertion to run concurrently
+  //fork {
+    when (io.sigA) {
+      for (i <- 0 until cycles) {
+        // Den helt generelle assertion
+        when (cond){
+          io.testp := true.B
+        } .otherwise {
+          System.out.println(message) //Printer uanset
+          io.testp := false.B
+        }
       }
     }
+  //}
+
+// Chain assertions, the heck?
+// Decoupled: ready-valid handshake
+
+
+
+
+  /*def testfunktion () = {
+  fork {
+    for (i <- 0 until cycles) {}
+    // Need some sort of break
   }
+  for (i <- 0 until cycles) {
+  // Den helt generelle assertion
+    when (cond){
+      io.testp := true.B
+    } .otherwise {
+      System.out.println(message) //Printer uanset
+      io.testp := false.B
+    }
+  }*/
 }
 
+
+
+
+
 object Main extends App {}
+
+/* Legacy, this stuff works
+class conAssert(cond: Bool, cycles: Int, message: String) extends Module{
+  val io = IO(new Bundle {
+    val sigA = Input(Bool())
+    val testp = Output(Bool())
+  })
+  io.testp := false.B
+  // The use of fork{} enables the assertion to run concurrently
+  //fork {
+    when (io.sigA) {
+      for (i <- 0 until cycles) {
+        // Den helt generelle assertion
+        when (cond){
+          io.testp := true.B
+        } .otherwise {
+          println(message) //Printer uanset
+          io.testp := false.B
+        }
+      }
+    }
+  //}
+}
+*/
--- assertion/src/test/scala/conAssertTest.scala
+++ assertion/src/test/scala/conAssertTest.scala
@@ -5,8 +5,8 @@ import org.scalatest._
 class conAssertTest extends FlatSpec with ChiselScalatestTester with Matchers {
     behavior of "The conAssert"
 
-    it should "test" in {
-        test(new conAssert(true.B, 10, "Halløjsa")) {
+    it should "test true condition" in {
+        test(new conAssert(true.B, 10, "Error")) {
             dut => {
                 dut.io.sigA.poke(true.B)
                 dut.clock.step(1)
@@ -14,4 +14,75 @@ class conAssertTest extends FlatSpec with ChiselScalatestTester with Matchers {
             }
         }
     }
+    /*it should "test false condition" in {
+        test(new conAssert(false.B, 10, "Error")) {
+            dut => {
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(false.B)
+            }
+        }
+    }
+    it should "fail" in {
+        test(new conAssert(true.B, 10, "Error")) {
+            dut => {
+                dut.io.sigA.poke(false.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+            }
+        }
+    }
+    it should "test changing condition after 4" in {
+        test(new conAssert(true.B, 10, "Error")) {
+            dut => {
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+                dut.clock.step(4)
+                dut.io.sigA.poke(false.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+            }
+        }
+    }
+    it should " fail test changing condition after 1" in {
+        test(new conAssert(true.B, 10, "Error")) {
+            dut => {
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+                dut.clock.step(1)
+                dut.io.sigA.poke(false.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+            }
+        }
+    }
+    it should "test changing condition after 1 and then back" in {
+        test(new conAssert(true.B, 10, "Error")) {
+            dut => {
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+                dut.clock.step(1)
+                dut.io.sigA.poke(false.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(false.B)
+                dut.clock.step(1)
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+            }
+        }
+    }
+    it should "be false due to too many cycles" in {
+        test(new conAssert(true.B, 10, "Error")) {
+            dut => {
+                dut.clock.step(11)
+                dut.io.sigA.poke(true.B)
+                dut.clock.step(1)
+                dut.io.testp.expect(true.B)
+            }
+        }
+    }*/
 }
